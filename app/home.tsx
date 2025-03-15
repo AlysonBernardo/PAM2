08828f7bbc62ce462896b67cba8120cb85d4f724
AlysonBernardo <alyson.santos6@etec.sp.gov.br>
2025-03-14 21:44:20 -0300

    Base files structure to study in the next classes

---
+++ app/home.tsx
@@ -0,0 +1,14 @@
+import { useRouter } from "expo-router";
+import { Button, Text, View } from "react-native";
+
+export default function Home(){
+    const router = useRouter();
+
+    return(
+        <View style={{flex:1, justifyContent:"center", alignItems:"center"}}>
+            <Text>Página Home</Text>
+            <Button title="Ver detales"
+            onPress={() => router.push('/details')}/>
+        </View>
+    );
+}
